9c3fa6375895a16cc13b2c34f8ca4e924b4b561d
Hardelele <i.lapshina@mail.ru>
2019-08-20 00:14:53 +0300

    Add and comment Stage.java, mkdir DesignPatterns.Singleton.

---
+++ src/DesignPatterns/Singleton/Stage.java
@@ -0,0 +1,32 @@
+package DesignPatterns.Singleton;
+
+    /* Паттерн проектирования Singleton с приминением метода getInstance:
+     * "инициализация по требованию владельца" Подробнее о getInstance:
+     * http://mng.bz/IGYx.
+     */
+
+public class Stage {
+
+    /*
+     * Приватный конструктор класса Stage
+     */
+
+    private Stage() { }
+
+    /*
+     * Приватный Singleton класс, отвечающий за создание instance'а класса Stage
+     */
+
+    private static class StageSingletonHolder {
+        static Stage instance = new Stage(); // Создание экземпляра
+    }
+
+    /*
+     * Метод getInstance, возвращающий экземпляр
+     */
+
+    public static Stage getInstance() {
+        return StageSingletonHolder.instance; // Возвращает экземпляр
+    }
+
+}
